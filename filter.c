2e8783cac341b0ae107585e1ae888e151756c2a3
mplandersen <mplandersen@gmail.com>
2023-10-18 10:30:03 +0200

    testing commit

--- filter.c
+++ filter.c
@@ -7,7 +7,7 @@
 int main(int argc, char *argv[])
 {
     // Define allowable filters
-    char *filters = "begr";
+    char *filters = "begra";
 
     // Get filter flag and check validity
     char filter = getopt(argc, argv, filters);
