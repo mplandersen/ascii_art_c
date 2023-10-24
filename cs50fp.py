4b2ebdae8a18545c4bf17c964af0003901f2762e
mplandersen <mplandersen@gmail.com>
2023-10-24 09:26:16 +0200

    updated readme

---
+++ cs50fp.py
@@ -0,0 +1,11 @@
+ascii = "@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,^`'."
+
+ascii = list(ascii)
+
+ascii.reverse()
+
+asciinew = [' ','.', '`', '^', '"', ',', ':', ';', 'I', 'l', '!', 'i', '>', '<', '~', '+', '_', '-', '?', ']', '[', '}', '{', '1', ')', '(', '|', '\\', '/', 't', 'f', 'j', 'r', 'x', 'n', 'u', 'v', 'c', 'z', 'X', 'Y', 'U', 'J', 'C', 'L', 'Q', '0', 'O', 'Z', 'm', 'w', 'q', 'p', 'd', 'b', 'k', 'h', 'a', 'o', '*', '#', 'M', 'W', '&', '8', '%', 'B', '@']
+
+asciinew.reverse()
+
+print(asciinew)
